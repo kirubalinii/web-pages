316a4a44801dacf65fecc8c14b07517f31044b7a
kirubalinii <kirubaliniganesan1610@gmail.com>
2025-01-28 16:25:59 +0530

    HTML , CSS, JS

---
+++ index.js
@@ -0,0 +1,14 @@
+searchicons=document.getElementById("searchicon");
+searchbar=document.querySelector(".search");
+
+searchicons.addEventListener('click',()=>{
+    searchbar.classlist.add("sub")
+
+    searchbar.classlist.remove("sub")
+    searchbar.classlist.remove("sub")
+    searchbar.classlist.remove("sub")
+    
+
+})
+
+
